e91daf46cd4687a96a508c9b30c829c846e95560
Ulanbek Bolotbekov <drreal@mail.ru>
2021-12-29 20:41:45 +0300

    Добавьте файлы проекта.

---
+++ ArrayValues.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ArrayOfRandomNegativeNumbers
+{
+    public static class ArrayValues
+    {
+        public static void FillArray(int[] nums)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rand.Next(-5, 6);
+            }
+        }
+
+        public static int[] FindNegativeNumbers(int[] nums)
+        {
+            return nums.Where(i => i < 0).ToArray();
+        }
+
+        public static void SortArray(int[] nums)
+        {
+            for (var i = 0; i < nums.Length / 2; i++)
+            {
+                var swapFlag = false;
+                for (var j = i; j < nums.Length - i - 1; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        int t = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = t;
+                        swapFlag = true;
+                    }
+                }
+                for (var j = nums.Length - 2 - i; j > i; j--)
+                {
+                    if (nums[j - 1] > nums[j])
+                    {
+                        int t = nums[j];
+                        nums[j] = nums[j - 1];
+                        nums[j - 1] = t;
+                        swapFlag = true;
+                    }
+                }
+                if (!swapFlag)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayOfRandomNegativeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            ArrayValues.FillArray(numbers);
+            Console.WriteLine("values of array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i]);
+            }
+            Console.WriteLine("\nvalues of new array with negative numbers:");
+            int[] newnumbers = ArrayValues.FindNegativeNumbers(numbers);
+            for (int i = 0; i < newnumbers.Length; i++)
+            {
+                Console.Write(newnumbers[i]);
+            }
+            Console.WriteLine("\nsorted array:");
+            ArrayValues.SortArray(newnumbers);
+            for (int i = 0; i < newnumbers.Length; i++)
+            {
+                Console.Write(newnumbers[i]);
+            }
+        }
+    }
+}
